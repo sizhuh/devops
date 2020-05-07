055768976e8bfe7a457a54de7a600f0f1ae723ff
Sizhu Hicks <sizhu@apple.com>
2020-05-07 14:32:06 -0700

    add new method

--- python_question/python_question.py
+++ python_question/python_question.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python
 
-def reverse_word_of_a_string(file):
+def check_constraints(file):
     with open(file, "r") as f:
         lines = f.readlines()
-
         # add constraints
         # number of testcases should be 1<=T<=10
         t = int(lines[0])
@@ -20,13 +19,43 @@ def reverse_word_of_a_string(file):
         msg = "Mismatch number of testcases {} and lengh of strings {}".format(t,l)
         assert t == l, msg
 
-        with open("output_file.txt", "w") as output_file:
-            for line in lines[1:]:
-                original_word = line.strip("\n").split(".")
-                for word in original_word:
-                    reverse_word = word[::-1]
-                    line = line.replace(word, reverse_word)
-                    new_line = line.replace(word, reverse_word)
-                output_file.write(new_line)
+        return lines
 
-reverse_word_of_a_string(file)
+def reverse_word_of_a_string(file):
+    lines = check_constraints(file)
+    with open("output_file.txt", "wa") as output_file:
+        # start read 2nd line in the file
+        for line in lines[1:]:
+            # sptrip newline and spearte word by speartor "."
+            original_word = line.strip("\n").split(".")
+
+            # start reverse word in each line
+            reverse_word = [word[::-1] for word in original_word]
+
+            # join the list of reversed words use "."
+            new_line = ".".join(reverse_word) + '\n'
+
+            # append the new line to the file
+            output_file.write(new_line)
+
+
+def reverse_word_of_a_string_use_replace_method(file):
+
+    # check constraints for the given file, assert errors
+    lines = check_constraints(file)
+    with open("output_file.txt", "w") as output_file:
+        for line in lines[1:]:
+            original_word = line.strip("\n").split(".")
+            for word in original_word:
+                reverse_word = word[::-1]
+
+                # replace original word in the line by reserved word one by one
+                line = line.replace(word, reverse_word)
+
+                # get the final reversed string
+                new_line = line.replace(word, reverse_word)
+            output_file.write(new_line)
+
+file = "/Users/sizhuhicks/Documents/ucsc_class/DevOps/devops/python_question/test_python_question.txt"
+#reverse_word_of_a_string(file)
+reverse_word_of_a_string_use_replace_method(file)
